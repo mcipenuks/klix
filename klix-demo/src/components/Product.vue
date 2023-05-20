dc1b03f33b7a03b6a7592b9b5f64194626c1f7a1
unknown <mcipenuks@gmail.com>
2023-05-20 21:25:17 +0300

    add product component

---
+++ klix-demo/src/components/Product.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+    title: 'Product',
+    price: 0,
+    image: '',
+});
+</script>
+
+<template>
+    <div class="product">
+        <img class="image" :src="image" alt="">   
+        <div class="title">{{ title }}</div>
+        <div class="price">{{ price }}</div>  
+    </div>
+</template>
+
+<style scoped>
+.image {
+    max-width: 100%;
+}
+</style>
