a41d27e43691a3bf314b59c62d03f283c97135bc
Mareks Cipenuks <mcipenuks@gmail.com>
2023-05-23 23:06:23 +0300

    Update formatting.spec.ts

--- klix-widget/src/utils/formatting/formatting.spec.ts
+++ klix-widget/src/utils/formatting/formatting.spec.ts
@@ -6,7 +6,7 @@ describe('formatText', () => {
         expect(formatText(undefined)).toEqual('');
     });
 
-    it('should return empty string if no was text provided', () => {
+    it('should replace placholders with provided arguments', () => {
         const text = 'Hello {0}, welcome to {1}'
         expect(formatText(text, 'Joe', 'Latvia')).toEqual('Hello Joe, welcome to Latvia');
     });
@@ -16,7 +16,7 @@ describe('formatText', () => {
         expect(formatText(text)).toEqual(text);
     });
 
-    it ('should replace placholder if provided argumetn is a string or a number', () => {
+    it ('should replace placholder if provided argument is a string or a number', () => {
         const text = "My name is {0} and I'm {1} years old"
         expect(formatText(text, 'Joe', 30)).toEqual("My name is Joe and I'm 30 years old");
     });
