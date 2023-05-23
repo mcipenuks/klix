53ee400098e6e3e1ff60619a2628bd1d131ae7d9
unknown <mcipenuks@gmail.com>
2023-05-23 21:09:03 +0300

    add language switcher for demo page

--- klix-demo/src/views/ProductView.vue
+++ klix-demo/src/views/ProductView.vue
@@ -1,14 +1,27 @@
 <script setup>
     import { RouterLink } from 'vue-router';
     import { useRoute } from 'vue-router'
-    import { ref, computed } from 'vue';
+    import { ref, computed, watch } from 'vue';
 
     const route = useRoute();
     const title = route.query.title;
     const price = ref(route.query.price);
     const image = route.query.image;
+    const language = ref('en');
+    const isKlixVisible = ref(true);
+
+    watch(language, () => {
+        isKlixVisible.value = false;
+        setTimeout(() => {
+            isKlixVisible.value = true;
+        }, 100);
+    })
 
     const priceInCents = computed(() => price.value * 100);
+
+    function selectLanguage(lang) {
+        language.value = lang;
+    }
 </script>
 
 <template>
@@ -22,7 +35,14 @@
         <div class="product-info">
             <h2>{{ title }}</h2>
             <div class="price">{{ price }} EUR</div>
-            <klix-pay-later :amount="priceInCents" language="en" brand-id="55b7d52b-020e-4143-bc69-3292b5002cf2"></klix-pay-later>
+            <klix-pay-later v-if="isKlixVisible" :amount="priceInCents" :language="language" brand-id="55b7d52b-020e-4143-bc69-3292b5002cf2"></klix-pay-later>
+            <div class="language-switch">
+                <h3>Current language: {{ language }}</h3>
+                <button @click="selectLanguage('en')">EN</button>
+                <button @click="selectLanguage('lv')">LV</button>
+                <button @click="selectLanguage('lt')">LT (not-supported)</button>
+            </div>
+            
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero aliquid minima omnis delectus incidunt recusandae distinctio, eos tenetur est exercitationem veritatis, cum excepturi! Dolore, ratione perspiciatis! Rem facilis praesentium, enim aspernatur velit facere tempora placeat et molestias explicabo corporis iste, quibusdam, quos ipsa. Illo, consectetur iste quibusdam, optio obcaecati excepturi, facilis cupiditate veniam aliquid quia minus est necessitatibus deserunt cumque sapiente? Minus assumenda dolore alias nulla at, aspernatur iste tempora dolorem libero quam? Dicta ea itaque minima voluptas vel veniam incidunt provident dolorum doloremque reiciendis illo qui repellendus ex autem veritatis a quia repellat deserunt, tenetur distinctio culpa. Quae corrupti minima consequuntur vitae porro temporibus sed, iusto inventore impedit amet fugit in ullam, dolore aut natus ab eligendi laudantium incidunt quis sapiente est at repellat! Pariatur dicta beatae veniam architecto libero unde eum nostrum dolorum dignissimos? Eum nostrum quidem tempora nemo qui aspernatur ipsa autem quos saepe deleniti eaque pariatur quam numquam cumque illo reiciendis, dolor fugit voluptatum voluptatibus excepturi eveniet magnam velit? Accusantium vero, neque minus laudantium repellendus sapiente exercitationem qui ipsum in nemo consequuntur minima nulla esse culpa? Iure est doloremque labore placeat aperiam libero molestias tempora similique aliquam assumenda, sunt incidunt accusantium earum eligendi fugiat, facere perferendis!</p>
         </div>
     </div>
@@ -56,8 +76,14 @@ h2 {
     margin-bottom: 10px;
 }
 p {
-    font-size: 18px;
+    font-size: 16px;
     line-height: 1.5;
+}
+.language-switch h3 {
+    margin-bottom: 5px;
+}
+.language-switch button {
+    margin-right: 5px;
 }
 @media screen and (max-width: 600px) {
     .container {
