2f1d2bf9a33f7386821615beea4d2eba27483610
unknown <mcipenuks@gmail.com>
2023-05-24 23:45:48 +0300

    add api return type

--- klix-widget/src/api/index.ts
+++ klix-widget/src/api/index.ts
@@ -1,6 +1,6 @@
 import { API_BASE_URL } from "../utils/env";
 
-export const fetchMonthlyPayment = async (payload: MonthlyPaymentPayload) => {
+export const fetchMonthlyPayment = async (payload: MonthlyPaymentPayload): Promise<MonthlyPaymentResponse>|null => {
     try {
         const response = await fetch(`${API_BASE_URL}/financing/monthly-payment?amount=${payload.amount}&language=${payload.language}&b=${payload.brandId}`);
 
